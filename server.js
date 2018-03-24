a58eee7844206f6dad0ba6891a07910cc7b8f581
Dan <Test@Tests-MacBook-Pro.local>
2018-03-24 11:22:52 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+// debugger;
+app.use((req,res,next) =>  {
+
+var time = new Date().toString();
+var log = `${time}: ${req.method}  ${req.url}`
+console.log(log);
+fs.appendFile('server.log', log + '\n', (err) => {
+  if (err) {
+    console.log('Unable to append to server.log');
+  }
+});
+next();
+});
+
+// app.use((req,res,next) => {
+
+    // res.render('maintnance.hbs', {
+      // pageTitle: 'maintnance page',
+      // welcomeMessage:"Sorry for the inconvinient"
+    // })
+
+// });
+
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase()
+})
+
+app.get('/', (req,res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home page',
+    welcomeMessage:"Welcome to Hadars Kingdom"
+  })
+});
+
+app.get('/about',(req,res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About page',
+  })
+});
+
+app.get('/bad' ,(req,res) => {
+  res.send({
+    error:"Unable to reach url"
+  })
+});
+
+app.listen(3000, () => {
+  console.log('Servers runs on port 3000');
+});
